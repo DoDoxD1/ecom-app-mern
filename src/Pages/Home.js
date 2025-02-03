f8d738e54b67c00a9cf8ff74cbdc414ee4a4a39f
= <=>
2025-02-03 10:01:49 +0530

    homeproducts side banner done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,46 @@ const Home = () => {
   return (
     <div className="homeBannerSection">
       <Homebanner />
+      <section className="homeProducts">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="banner">
+                <div className="content">
+                  <div className="top">
+                    <p>Bacola Natural Foods</p>
+                  </div>
+                  <div className="heading">
+                    <h1>Special Organic </h1>
+                    <h1 className="text-bold">Roats Burger</h1>
+                  </div>
+                  <div className="subheading">
+                    <p>only-from</p>
+                  </div>
+                  <div className="price d-flex align-items-baseline">
+                    <span className="">$14.35</span>
+                  </div>
+                </div>
+                <img
+                  src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/banner-box.jpg"
+                  alt=""
+                  className="cursor"
+                />
+              </div>
+            </div>
+            <div className="col-md-9 productRow">
+              <div className="d-flex align-items-center">
+                <div className="info">
+                  <h3 className="mb-0 hd">BEST SELLERS</h3>
+                  <p className="text-light text-sml">
+                    Do not miss the current offers until the end of March.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
