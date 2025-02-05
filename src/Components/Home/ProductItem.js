b52a575f784da1533c118619e63d7b33d09f597b
= <=>
2025-02-05 18:46:51 +0530

    Best sellers section done

--- src/Components/Home/ProductItem.js
+++ src/Components/Home/ProductItem.js
@@ -1,15 +1,24 @@
 import { BsArrowsFullscreen } from "react-icons/bs";
-
+import { FaRegHeart } from "react-icons/fa";
 import Rating from "@mui/material/Rating";
+import { Button } from "@mui/material";
 
 function ProductItem(props) {
   return (
     <div className="item productItem">
-      <div className="imgWrapper">
+      <div className="imgWrapper mb-1">
         <img src={props.item.img} alt="" className="w-100" />
       </div>
       <div className="description">
         <span className="discount">{props.item.discount}%</span>
+        <div className="actions d-flex flex-column gap-2">
+          <Button className="shadow-sm d-flex justify-content-center align-items-center">
+            <BsArrowsFullscreen />
+          </Button>
+          <Button className="shadow-sm d-flex justify-content-center align-items-center">
+            <FaRegHeart />
+          </Button>
+        </div>
         <h6 className="title">{props.item.title}</h6>
         <span className="d-block inStock mb-1">IN STOCK</span>
         <Rating
